1a942bb387ab2be423b7cb7482daf09adc5c3de6
Akshay Kumar <akshaykkumar090@gmail.com>
2019-03-12 16:13:56 +0530

    another commit

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 # Presentation Layer of Assignment Management System
-import BLL
+from BLL import Student
 from myDB import *
 
 
 
 def main():
     print('*** AMS ***')
+    while True:
+        try:
+            ch = input('''User Type:
+        1. Admin
+        2. Teacher
+        3. Student
+            your choise: ''')
+            if ch == '1':
+                print('Admin')
+                for a in adminList:
+                    print(a)
+
+            elif ch == '2':
+                print('Teacher')
+                for t in teacherList:
+                    print(t)
+
+            elif ch == '3':
+                print('Student')
+                for s in studentList:
+                    print(s)
+
+            else:
+                print('Invalid Input!')
+        finally:
+            ex = input('Wanna EXIT? Y/N: ')
+            if ex == 'y' or ex == 'Y':
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- myDB.py
+++ myDB.py
@@ -1,10 +1,20 @@
 import mysql.connector
+try:
+    con = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            passwd='admin',
+            database='studentAttend'
+        )
 
-con = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='admin',
-        database='studentAttend'
-    )
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM admin')
+    adminList = cursor.fetchall()
+    cursor.execute('SELECT * FROM teacher')
+    teacherList = cursor.fetchall()
+    cursor.execute('SELECT * FROM student')
+    studentList = cursor.fetchall()
 
-cursor = con.cursor(prepared=True)
+except Exception as e:
+    print('Database Error: ',e)
+
